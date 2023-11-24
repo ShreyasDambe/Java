ad78fe84829338a5f62629cc648133828161124a
ShreyasDambe <101385887+ShreyasDambe@users.noreply.github.com>
2023-11-24 20:35:01 +0530

    Add files via upload

---
+++ java/DD.java
@@ -0,0 +1,9 @@
+//import java.lang.*;
+
+class DD
+{
+    public static void main (String[] args)
+    {
+        System.out.println("Linus Infosystems");
+    }
+}
---
+++ java/Java.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+
+class Java
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Linus Infosystems with Shreyas");
+    }
+}
