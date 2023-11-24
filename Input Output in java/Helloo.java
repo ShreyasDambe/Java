b0c5052d7df6e4f59483c7dc81d250c3b6a82cb1
ShreyasDambe <101385887+ShreyasDambe@users.noreply.github.com>
2023-11-24 20:34:27 +0530

    Add files via upload

---
+++ Input Output in java/Helloo.java
@@ -0,0 +1,25 @@
+//Hello.java
+
+import java.util.Scanner;
+
+class Helloo
+{
+    public static void main(String args[])
+    {
+        Scanner Sobj =  new Scanner(System.in);
+
+        System.out.println("Enter your Name:");
+        String Name = Sobj.nextLine();
+
+        System.out.println("Enter your Age:");
+        byte iAge = Sobj.nextByte();
+
+        System.out.println("Enter your Percentage:");
+        float fPer = Sobj.nextFloat();
+
+        System.out.println("Your Name is:"+Name);
+        System.out.println("Your Age is:"+iAge);
+        System.out.println("Your Percentage is:"+fPer);
+
+    }
+}
